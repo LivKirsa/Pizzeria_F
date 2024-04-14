d499758eb0f394260355d16f607d28d0b2d09e48
carobraaa <166133374+carobraaa@users.noreply.github.com>
2024-04-14 15:22:00 +0200

    Update MainPizzeria.java
    Hej alle. Her er noget af den kode, jeg tror vi skal have med, men jeg har ikke fået den til at køre med tid og fejlmeldinger, så måske kan noget bruges :-)

--- MainPizzeria.java
+++ MainPizzeria.java
@@ -1,24 +1,40 @@
 import java.util.Scanner;
 import java.sql.*;
 import java.util.Arrays;
+import java.util.InputMismatchException;
 
 //Class - menu
 
 public class MainPizzeria{
 public static void main (String [] args){
 
-   System.out.println("Hvilken pizza vil du bestille?");
-   Scanner console = new Scanner(System.in);
-   
-   //console.nextInt();
+Scanner console = new Scanner(System.in); // Scanner-kode her (f.eks. nextInt(), nextLine() osv.)
+ 
+ Menu menu = new Menu();
+   menu.printList();
 
+try {
+   System.out.println("Hvilken pizza vil du bestille?");
+   String menuList = console.nextLine();
+   System.out.println("Du intastede:"+ menuList);
    System.out.println("Hvad tid vil du hente den?");
      //console.nextInt();
+Order pizzaorder = new Order("Caroline", 24, X);
+   } catch (InputMismatchException e){ // e.getClass() til at finde ud af, hvilken type fejl der er opstået.
+    System.out.println("Fejl: Ugyldig indtastning. Indtast et tal.");
+    console.next(); // Ignorer den ugyldige indtastning
+    
+   } catch (Exception e) {
+  System.out.println("Uventet fejl: " + e.getMessage());
+   e.printStackTrace(); // Vis detaljeret fejlinformation
+
      
-   Menu menu = new Menu();
-   menu.printList();
+    
    
-   //System.out.println("Pizza Menu");
+   
+   }
+}
+ }     //System.out.println("Pizza Menu");
    
 //initalize bestilling object, menu, database
 
@@ -28,9 +44,7 @@ public static void main (String [] args){
 //method i bestilling som caller pizza i menu i arraylist
 //bestilling henter data fra menu og database
 
-}
 
 //call methods from bestilling
 //call pizza methods
 
-}
